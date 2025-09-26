2baf43400df80c9b9faa4fc3a069b803c51f2426
Vaibh-v <165192898+Vaibh-v@users.noreply.github.com>
2025-09-26 23:31:33 +0530

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,9 +2,14 @@ import { PrismaClient } from "@prisma/client";
 
 declare global {
   // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
+  var __prisma__: PrismaClient | undefined;
 }
 
-export const prisma = global.prisma ?? new PrismaClient();
+const prisma = global.__prisma__ ?? new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+// In dev, keep a single instance across HMR reloads
+if (process.env.NODE_ENV !== "production") {
+  global.__prisma__ = prisma;
+}
+
+export default prisma;
