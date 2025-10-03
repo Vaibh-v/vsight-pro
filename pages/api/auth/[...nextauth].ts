d82edf4d3fd4a1b70d0711b7460fcd87e75ffb4b
Vaibh-v <165192898+Vaibh-v@users.noreply.github.com>
2025-10-03 23:32:47 +0530

    Delete pages directory

--- pages/api/auth/[...nextauth].ts
+++
@@ -1,8 +0,0 @@
-// pages/api/auth/[...nextauth].ts
-import NextAuth from "next-auth";
-
-export const authOptions = {
-  providers: [],
-  session: { strategy: "jwt" as const },
-};
-export default NextAuth(authOptions as any);
