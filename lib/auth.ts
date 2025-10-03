0feba9e572688ffa4c9b6315f470af086c4fd836
Vaibh-v <165192898+Vaibh-v@users.noreply.github.com>
2025-10-03 23:32:27 +0530

    Delete lib directory

--- lib/auth.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest } from "next";
-import { getToken } from "next-auth/jwt";
-
-export async function requireGoogleAccessToken(req: NextApiRequest): Promise<string> {
-  const token = await getToken({ req });
-  const at = (token as any)?.access_token as string | undefined;
-  if (!at) throw new Error("No Google access token. Please sign in.");
-  return at;
-}
